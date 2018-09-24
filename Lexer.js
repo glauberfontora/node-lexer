cf5e7d8ac2ecb99a71de8538ae41b62d81160b3f
Glauber Fontora <glauber@q3m.com.br>
2018-09-23 23:52:01 -0300

    Implementação de automato.

--- Lexer.js
+++ Lexer.js
@@ -1,26 +1,208 @@
+const fs = require('fs')
+
 const TS = require('./lib/TS.js')
-let EOF
-let lookahead = 0
+const Token = require('./lib/Token.js')
+const Tag = require('./lib/Tag.js')
+
+let EOF, lookhead
+
+let code
+let pointer = 0
+
 let line = 1
 let column = 1
+
+let state
+let tokenName
+
+const main = () => {
+  const portugoloCode = fs.createReadStream('./segundo_portugolo.ptgl')
+
+  portugoloCode.on('data', item => {
+    code = item.toString('utf8').split('')
+    EOF = code.length
+
+    let token
+    
+    do {
+      state = 'S'
+      tokenName = []
+      token = nextToken()
+      console.log(token)
+    } while(token !== null && pointer <= EOF)
+
+  })
+
+  portugoloCode.on('end', () => {
+    console.log('Leitura completa')
+  })
+
+}
+
+main()
+
+const isDigit = char => Number.isInteger(parseInt(char))
+
+const isLetter = char => char.match(/[a-zA-Z]/) && char.match(/[a-zA-Z]/).length > 0
+
+const nextToken = () => {
+  lookhead = pointer !== EOF ? code[pointer] : 'EOF'
+  while(pointer < 50) {
+    lookhead = pointer !== EOF ? code[pointer] : 'EOF'
+    pointer++
+    switch (state) {
+      case 'S':
+        if (lookhead === 'EOF') {
+          return Token.newToken(Tag.getTagType(lookhead, false), lookhead, line, column)
+        } else if (lookhead === ' ' || lookhead === '\t' || lookhead === '\n' || lookhead === '\r') {
+          state = 'S'
+        } else if (
+          lookhead === ';'
+          || lookhead === '('
+          || lookhead === ')'
+          || lookhead === ','
+          || lookhead === '='
+          || lookhead === '*'
+          || lookhead === '-'
+          || lookhead === '+'
+        ) {
+          return Token.newToken(Tag.getTagType(lookhead, false), lookhead, line, column)
+        } else if (isDigit(lookhead)) {
+          pointer--
+          state = 1
+        } else if (isLetter(lookhead)) {
+          tokenName.push(lookhead)
+          state = 8
+        } else if (lookhead === '"') {
+          tokenName.push(lookhead)
+          state = 5
+        } else if (lookhead === '>') {
+          tokenName.push(lookhead)
+          state = 18
+        } else if (lookhead === '/') {
+          pointer--
+          state = 20
+        } else if (lookhead === '<') {
+          tokenName.push(lookhead)
+          state = 21
+        } else {
+          showError("Caractere invalido " + lookhead + " na linha " + line + " e coluna " + column)
+          return null;
+       }
+        break
+      case 1:
+        if (lookhead === '.') {
+          tokenName.push(lookhead)
+          state = 2
+        } else if (isDigit(lookhead)) {
+          tokenName.push(lookhead)
+        } else {
+          return Token.newToken('NUMERICO', tokenName.join(''), line, column)
+        }
+        break
+      case 2:
+        if (isDigit(lookhead)) {
+          tokenName.push(lookhead)
+          state = 3
+        } else {
+          showError("Caractere invalido " + lookhead + " na linha " + line + " e coluna " + column)
+        }
+      break
+      case 3:
+        if (isDigit(lookhead)) {
+          tokenName.push(lookhead)
+        }
+        else {
+          pointer--
+          return Token.newToken('NUMERICO', tokenName.join(''), line, column)
+        }
+      break
+      case 5:
+        if (lookhead === '"') {
+          showError("String deve conter pelo menos um caractere. Erro na linha " + line + " coluna " + column)
+          return null;
+        }
+        else if (lookhead === '\n') {
+          showError("Padrao para [ConstString] invalido na linha " + line + " coluna " + column)
+          return null;
+        }
+        else if (lookahead === 'EOF') {
+          showError("String deve ser fechada com \" antes do fim de arquivo")
+          return null;
+        }
+        else {
+          tokenName.push(lookhead)
+          state = 6
+        }
+      break
+      case 6:
+        if (lookhead === '"') {
+          return Token.newToken('LITERAL', tokenName.join(''), line, column)
+        }
+        else if (lookhead === '\n') {
+          showError("Padrao para [ConstString] invalido na linha " + line + " coluna " + column)
+          return null;
+        }
+        else if (lookahead === 'EOF') {
+          showError("String deve ser fechada com \" antes do fim de arquivo")
+          return null;
+        }
+        else {
+          tokenName.push(lookhead)
+        }
+      break
+      case 8:
+        if (isDigit(lookhead) || isLetter(lookhead)) {
+          tokenName.push(lookhead)
+        }
+        else {
+          pointer--
+          return Token.newToken('ID', tokenName.join(''), line, column)
+        }
+      break
+      case 18:
+        if (lookhead === '=') {
+          tokenName.push(lookhead)
+          return Token.newToken(Tag.getTagType(tokenName.join(''), false), tokenName.join(''), line, column)
+        }
+        else {
+          pointer--
+          return Token.newToken(Tag.getTagType(tokenName.join(''), false), tokenName.join(''), line, column)
+        }
+      case 21:
+        if (lookhead === '=') {
+          tokenName.push(lookhead)
+          return Token.newToken(Tag.getTagType(tokenName.join(''), false), tokenName.join(''), line, column)
+        } else if (lookhead === '>') {
+          tokenName.push(lookhead)
+          return Token.newToken(Tag.getTagType(tokenName.join(''), false), tokenName.join(''), line, column)
+        } else if (lookhead === '-') {
+          tokenName.push(lookhead)
+          state = 25
+        } else {
+          pointer--
+          return Token.newToken(Tag.getTagType(tokenName.join(''), false), tokenName.join(''), line, column)
+        }
+      break
+      case 25:
+        if (lookhead === '-') {
+          tokenName.push(lookhead)
+          return Token.newToken(Tag.getTagType(tokenName.join(''), false), tokenName.join(''), line, column)
+        } 
+        // else {
+        //   state = 25
+        //   showError("Caractere invalido " + lookhead + " na linha " + line + " e coluna " + column)
+        //   return null
+        // }
+      break
+    }
+  }
+}
 
 const showError = mensagem => {
   console.log(`[Erro Lexico]: ${mensagem}`)
 }
 
-// const pointerBack = () => {
-//   try {
-//      // Não é necessário retornar o ponteiro em caso de Fim de Arquivo
-//      if(lookahead != END_OF_FILE) {
-//         instance_file.seek(instance_file.getFilePointer() - 1);
-//      }    
-//   }
-//   catch(IOException e) {
-//      System.out.println("Falha ao retornar a leitura\n" + e);
-//      System.exit(4);
-//   }
-// }
-
 // Imprimir tabela de símbolos
-console.log("Tabela de simbolos:")
-TS.getTS()
+// console.log("Tabela de simbolos:")
+// TS.getTS()
